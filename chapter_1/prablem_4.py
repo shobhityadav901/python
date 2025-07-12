db7065acff70913f8441820f5f0bfe7110466622
SHOBHIT YADAV <yadavdhakar4@gmail.com>
2025-07-12 11:18:55 +0530

    Delete chapter_1 directory

--- chapter_1/prablem_4.py
+++
@@ -1,6 +0,0 @@
-import os 
-# Function to get the list of files in a directory
-directory_path = '/'
-contents = os.listdir(directory_path) # Get the list of files and directories in the specified path 
-for item in contents:
-    print(item)
--- chapter_1/problem_3.py
+++
@@ -1,4 +0,0 @@
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("hello i am shobhit i am a software engineer")
-engine.runAndWait()
